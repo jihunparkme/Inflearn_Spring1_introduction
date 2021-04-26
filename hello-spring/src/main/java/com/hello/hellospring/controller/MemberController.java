f224e65dd668ce5d37ee3edca20cb9898ae60066
jihunparkme <jihunpark.tech@gmail.com>
2021-04-26 22:51:32 +0900

    회원 웹 기능 - 조회

--- hello-spring/src/main/java/com/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/com/hello/hellospring/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.hello.hellospring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -63,7 +66,14 @@ public class MemberController {
 		member.setName(form.getName());
 
 		memberService.join(member);
-		
+
 		return "redirect:/";
+	}
+
+	@GetMapping(value = "/members")
+	public String list(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+		return "members/memberList";
 	}
 }
