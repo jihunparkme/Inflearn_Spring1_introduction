6eaef8f56300aef0fc89d2dab166f4c0f2840a19
jihunparkme <jihunpark.tech@gmail.com>
2021-04-26 21:29:14 +0900

    maven example

---
+++ mavenexample/src/main/java/com/example/mavenexample/MavenexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenexampleApplication.class, args);
+	}
+
+}
