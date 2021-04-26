215a3b6b05efa06ff1d9f48e61d8d0c26c9e87d9
jihunparkme <jihunpark.tech@gmail.com>
2021-04-26 21:28:51 +0900

    gradle example

---
+++ gradleExample/src/test/java/com/example/gradleExample/GradleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradleExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
