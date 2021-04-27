f2e43d32a713ce06533c8dbc1a4802a09d96ae31
jihunparkme <jihunpark.tech@gmail.com>
2021-04-27 22:45:39 +0900

    Spring 통합 테스트

--- hello-spring/src/test/java/com/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/com/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -3,42 +3,38 @@ package com.hello.hellospring.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hello.hellospring.domain.Member;
-import com.hello.hellospring.repository.MemoryMemberRepository;
+import com.hello.hellospring.repository.MemberRepository;
 
 /*
- * 단위 테스트
- * 순수한 단위 테스트가 훨씬 좋은 테스트일 가능성이 높다!
+ * Spring 통합 테스트
+ * 
+ * @SpringBootTest : 스프링 컨테이너와 테스트를 함께 실행
+ * @Transactional : 테스트 케이스에 이 애노테이션이 있으면, 테스트 시작 전에 트랜잭션을 시작하고, 테스트 완료 후에 항상 롤백 
+ *   				(각 테스트마다 적용)   
  */
-class MemberServiceTest {
+@SpringBootTest
+@Transactional 
+class MemberServiceIntegrationTest {
 	
-	MemberService memberService;
-	MemoryMemberRepository memberRepository;
-
-	@BeforeEach
-	public void beforeEach() {
-		memberRepository = new MemoryMemberRepository();
-		memberService = new MemberService(memberRepository);
-	}
-
-	@AfterEach
-	public void afterEach() {
-		memberRepository.clearStore();
-	}
+	@Autowired MemberService memberService;
+	@Autowired MemberRepository memberRepository;
 
 	@Test
 	public void 회원가입() throws Exception {
 		// Given
 		Member member = new Member();
 		member.setName("hello");
+		
 		// When
 		Long saveId = memberService.join(member);
+		
 		// Then
 		Member findMember = memberRepository.findById(saveId).get();
 		assertEquals(member.getName(), findMember.getName());
@@ -51,18 +47,11 @@ class MemberServiceTest {
 		member1.setName("spring");
 		Member member2 = new Member();
 		member2.setName("spring");
+		
 		// When
 		memberService.join(member1);
-		IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));// 예외가 발생해야 한다.
+		IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));// 예외 발생
 		
 		assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-		/*
-		try {
-			memberService.join(member2);
-			fail();
-		} catch (IllegalStateException e) {
-			assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-		}
-		*/
 	}
 }
